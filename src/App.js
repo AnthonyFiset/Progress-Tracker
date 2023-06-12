3d7ec7e1284c7a081a702e06cee44504ec701fb8
AnthonyFiset <tonyfise@my.yorku.ca>
2023-06-12 11:40:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+function ProgressTracker() {
+  const [goal, setGoal] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+  const handleGoalChange = (e) => {
+    setGoal(e.target.value);
+  };
+
+  const incrementProgress = () => {
+    if (progress < goal) {
+      setProgress(progress + 1);
+    } else {
+      alert("Congratulations! You've reached your goal.");
+    }
+  };
+
+  const resetProgress = () => {
+    setProgress(0);
+    setGoal(0);
+  };
+
+  const progressBarWidth = goal !== 0 ? (progress / goal) * 100 : 0;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Job Applications</h1>
+      <input 
+        type="number" 
+        value={goal} 
+        onChange={handleGoalChange} 
+        placeholder="Enter your goal"
+      />
+      <button onClick={incrementProgress}>Applied</button>
+      <button onClick={resetProgress}>Reset</button>
+      <div className="progressBarContainer">
+        <div className="progressBar" style={{width: `${progressBarWidth}%`}}></div>
+      </div>
+      <p>Job Applications: {progress} out of {goal}</p>
     </div>
   );
 }
 
-export default App;
+export default ProgressTracker;
